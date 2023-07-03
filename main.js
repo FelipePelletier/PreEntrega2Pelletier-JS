62c5e007283d64837e75b0a06066e928ede85477
Felipe Pelletier <felipelletier@gmail.com>
2023-07-03 15:25:53 -0300

    creo funcion para agregar alumno con sus notas y calcular promedio

--- main.js
+++ main.js
@@ -29,3 +29,35 @@ while (menu) {
     alert("Opcion invalida. Por favor seleccione una opcion valida");
   }
 }
+
+function agregarAlumno() {
+  let nombre = prompt("Ingrese el nombre del alumno:");
+  while (!letrasRegex.test(nombre)) {
+    alert("El nombre solo debe tener letras. Pruebe nuevamente.");
+    nombre = prompt("Ingrese el nombre del alumno:");
+  }
+
+  const notas = [];
+  let nota1 = prompt("Ingrese la primer nota del alumno:");
+  while (!numerosRegex.test(nota1)) {
+    alert("La nota debe ser un numero. Pruebe nuevamente");
+    nota1 = prompt("Ingrese la primer nota del estudiante:");
+  }
+  notas.push(parseFloat(nota1));
+
+  let nota2 = prompt("Ingrese la segunda nota del alumno:");
+  while (!numerosRegex.test(nota1)) {
+    alert("La nota debe ser un numero. Pruebe nuevamente");
+    nota1 = prompt("Ingrese la segunda nota del estudiante:");
+  }
+  notas.push(parseFloat(nota2));
+
+  const promedio = calcularPromedio(notas);
+
+  alumnos.push({ nombre, notas, promedio });
+}
+
+function calcularPromedio(notas) {
+  const suma = notas.reduce((acumulador, nota) => acumulador + nota, 0);
+  return suma / notas.length;
+}
